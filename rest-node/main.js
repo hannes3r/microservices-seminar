f1db0b8dcb073594faa1aca6b68fd1037c6f4fff
hannes3r <hannes.dreyer@outlook.de>
2023-08-31 15:03:37 +0000

    final changes

--- rest-node/main.js
+++ rest-node/main.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const https = require('https')
+const fs = require('fs')
 const app = express()
 const port = 9501
 
@@ -12,6 +14,11 @@ app.get('/check-blacklist', (req, res) => {
   res.send(JSON.stringify({
     is_blacklisted: blacklist.indexOf(req.query.name) == -1 ? false : true
   }))
+})
+
+app.get('/check-blacklist-plain', (req, res) => {
+  console.log(req.query.name)
+  res.send(blacklist.indexOf(req.query.name) == -1 ? "false" : "true")
 })
 
 app.listen(port, () => {
